d3ee83c79264237a913587279105751d25d03fe0
Aseka <grooves97@gmail.com>
2019-04-08 21:29:45 +0600

    Создал классы и написал команды ado.net

---
+++ ADO.HW2.CONSOLE/ADO.HW2.CONSOLE/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADO.HW2.Models;
+
+namespace ADO.HW2.CONSOLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            dataService.CreateGruppaTable();
+            //ДА ПРИБУДЕТ С ВАМИ СИЛА УЧИТЕЛЬ!
+            //ВАШ ПАДАВАН АСЛАН!
+        }
+    }
+}
